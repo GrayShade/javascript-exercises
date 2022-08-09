35494666ce8fa823183e38792fad40bac821a256
Gray <silicongray@gmail.com>
2022-08-09 19:07:24 +0500

    Revert "complete & verify first seven exercises on 09-AUG-22"
    This reverts commit 6267494778cd5d8f7ed9941cf9d516972353d65a.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,29 +1,6 @@
-const repeatString = function (string, times) {
+const repeatString = function() {
 
-    let result = '';
-
-    if (times < 0) return 'ERROR';
-
-    for (i = 1; i <= times; i++) {
-        result += string;
-    }
-    return result;
 };
-
-//  official solution:
-/* const repeatString = function(word, times) {
-  if (times < 0) return 'ERROR';
-  let string = '';
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
-};
-
-module.exports = repeatString; */
-
 
 // Do not edit below this line
 module.exports = repeatString;
-
-
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,26 +1,6 @@
-const leapYears = function(year) {
-    if(year % 4 == 0) {
-        if(year % 100 == 0) {
-            if(year % 400 == 0) {
-                return true;
-            } else return false;
-        } 
-        return true;
+const leapYears = function() {
+
 };
-    // if none, :
-    return false;
-}
-
-/* 
-
-official solution:
-const leapYears = function(year) {
-  return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
-};
-
-module.exports = leapYears;
-
-*/
 
 // Do not edit below this line
 module.exports = leapYears;
