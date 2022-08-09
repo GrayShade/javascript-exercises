2dd06109478d716a8909ef7007f437b1a0cad06a
Gray <silicongray@gmail.com>
2022-08-09 19:40:16 +0500

    Revert "complete & verify first seven exercises on 09-AUG-22" & recommit
    This reverts commit 35494666ce8fa823183e38792fad40bac821a256.

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,14 @@
-const helloWorld = function() {
-  return ''
+const helloWorld = function () {
+  return 'Hello, World!'
 };
 
+//  official solution:
+/* const helloWorld = function() {
+  return 'Hello, World!';
+};
+
+module.exports = helloWorld; */
+
 module.exports = helloWorld;
+
+
